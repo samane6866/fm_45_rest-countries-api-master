c11c9ea744cb0d2af202aed11b45249af6cb172d
samane6866 <samane.bahramian@gmail.com>
2023-05-11 11:05:43 +0200

    some change done

--- js/main.js
+++ js/main.js
@@ -11,67 +11,146 @@ function init() {
 }
 
 window.onload = init();
-let removeClass = document
-  .querySelector("#country-details")
-  .classList.remove("display-none");
+// let removeClass = document
+//   .querySelector("#country-details")
+//   .classList.remove("display-none");
+// async function getCountry() {
+//   const respuesta = await fetch("https://restcountries.com/v3.1/all");
+//   console.log("hola");
+//   const data = await respuesta.json();
+//   // console.log(data);
+//   const countryDetails = document.querySelector("#country-details");
+//   const countryDetailsContainer = document.querySelector(
+//     "#country-details-container"
+//   );
+//   const countriesHtml = data.map((item) => {
+//     console.log(item.name.common);
+//     return;
+//     ` <section id="country-details">
+//         <button id="btn-leave-details" aria-label="back to main page">
+//           <i class="bi bi-arrow-left"></i> Back
+//         </button>
+
+//         <div id="country-details-container">
+//           <div>
+//             <img id="country-detail-flag" src="#" alt="flag not loaded" />
+//           </div>
+
+//           <div class="country-detail-text">
+//             <h2 id="country-detail-name">${item.name.common}</h2>
+
+//             <div class="country-detail-text-container">
+//               <div class="text-container1">
+//                 <p>
+//                   <span>Native Name: </span>
+//                   <span id="detail-native-name"></span>
+//                 </p>
+//                 <p>
+//                   <span>Population: </span>
+//                   <span id="detail-population"></span>
+//                 </p>
+//                 <p><span>Region: </span><span id="detail-region"></span></p>
+//                 <p>
+//                   <span>Sub Region: </span>
+//                   <span id="detail-sub-region"></span>
+//                 </p>
+//                 <p><span>Capital: </span><span id="detail-capital">Ul</span></p>
+//               </div>
+//               <div class="text-container2">
+//                 <p>
+//                   <span>Top Level Domain: </span>
+//                   <span id="detail-tld"></span>
+//                 </p>
+//                 <p>
+//                   <span>Currencies: </span>
+//                   <span id="detail-currency"></span>
+//                 </p>
+//                 <p>
+//                   <span>Languages: </span>
+//                   <span id="detail-languages"></span>
+//                 </p>
+//               </div>
+//             </div>
+
+//             <h3>Border Countries:</h3>
+//             <div id="border-countries-container"></div>
+//           </div>
+//         </div>
+//       </section>`;
+//   });
+//   countryDetailsContainer.insertAdjacentHTML(
+//     "beforeend",
+//     countriesHtml.join("")
+//   );
+
+//   // Finalmente, removemos la clase "display-none" para mostrar el elemento #country-details
+//   countryDetails.classList.remove("display-none");
+// }
+// getCountry();
+
 async function getCountry() {
   const respuesta = await fetch("https://restcountries.com/v3.1/all");
-  console.log("hola");
   const data = await respuesta.json();
-  // console.log(data);
-  return data.forEach((item) => {
-    console.log(item.name.common);
-    ` <section id="country-details">
-        <button id="btn-leave-details" aria-label="back to main page">
-          <i class="bi bi-arrow-left"></i> Back
-        </button>
 
-        <div id="country-details-container">
-          <div>
-            <img id="country-detail-flag" src="#" alt="flag not loaded" />
+  const countryDetails = document.querySelector("#country-details");
+  const countryDetailsContainer = document.querySelector(
+    "#country-details-container"
+  );
+
+  // Aquí utilizamos el método map en lugar de forEach para crear un array con el contenido HTML para cada país
+  const countriesHtml = data.map((item) => {
+    console.log(item);
+    return `
+      <div>
+        <img src="${item.flags[0]}" alt="flag not loaded" />
+      </div>
+      <div class="country-detail-text">
+        <h2 id="country-detail-name">${item.name.common}</h2>
+        <div class="country-detail-text-container">
+          <div class="text-container1">
+            <p>
+              <span>Native Name: </span>
+              <span id="detail-native-name"></span>
+            </p>
+            <p>
+              <span>Population: </span>
+              <span id="detail-population"></span>
+            </p>
+            <p><span>Region: </span><span id="detail-region"></span></p>
+            <p>
+              <span>Sub Region: </span>
+              <span id="detail-sub-region"></span>
+            </p>
+            <p><span>Capital: </span><span id="detail-capital">${item.capital}</span></p>
           </div>
-
-          <div class="country-detail-text">
-            <h2 id="country-detail-name">${item.name.common}</h2>
-
-            <div class="country-detail-text-container">
-              <div class="text-container1">
-                <p>
-                  <span>Native Name: </span>
-                  <span id="detail-native-name"></span>
-                </p>
-                <p>
-                  <span>Population: </span>
-                  <span id="detail-population"></span>
-                </p>
-                <p><span>Region: </span><span id="detail-region"></span></p>
-                <p>
-                  <span>Sub Region: </span>
-                  <span id="detail-sub-region"></span>
-                </p>
-                <p><span>Capital: </span><span id="detail-capital">Ul</span></p>
-              </div>
-              <div class="text-container2">
-                <p>
-                  <span>Top Level Domain: </span>
-                  <span id="detail-tld"></span>
-                </p>
-                <p>
-                  <span>Currencies: </span>
-                  <span id="detail-currency"></span>
-                </p>
-                <p>
-                  <span>Languages: </span>
-                  <span id="detail-languages"></span>
-                </p>
-              </div>
-            </div>
-
-            <h3>Border Countries:</h3>
-            <div id="border-countries-container"></div>
+          <div class="text-container2">
+            <p>
+              <span>Top Level Domain: </span>
+              <span id="detail-tld"></span>
+            </p>
+            <p>
+              <span>Currencies: </span>
+              <span id="detail-currency"></span>
+            </p>
+            <p>
+              <span>Languages: </span>
+              <span id="detail-languages"></span>
+            </p>
           </div>
         </div>
-      </section>`;
+        <h3>Border Countries:</h3>
+        <div id="border-countries-container"></div>
+      </div>
+    `;
   });
+
+  // Ahora agregamos el contenido HTML al elemento #country-details-container
+  countryDetailsContainer.insertAdjacentHTML(
+    "beforeend",
+    countriesHtml.join("")
+  );
+
+  // Finalmente, removemos la clase "display-none" para mostrar el elemento #country-details
+  countryDetails.classList.remove("display-none");
 }
 getCountry();
