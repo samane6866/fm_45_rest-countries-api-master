dbd9196d9ac6c99d0b9d7566e37d253cbaa9c21c
Dina <dina.yamanid@gmail.com>
2023-05-11 13:07:47 +0200

    searcher done

--- js/main.js
+++ js/main.js
@@ -11,86 +11,11 @@ function init() {
 }
 
 window.onload = init();
-// let removeClass = document
-//   .querySelector("#country-details")
-//   .classList.remove("display-none");
-// async function getCountry() {
-//   const respuesta = await fetch("https://restcountries.com/v3.1/all");
-//   console.log("hola");
-//   const data = await respuesta.json();
-//   // console.log(data);
-//   const countryDetails = document.querySelector("#country-details");
-//   const countryDetailsContainer = document.querySelector(
-//     "#country-details-container"
-//   );
-//   const countriesHtml = data.map((item) => {
-//     console.log(item.name.common);
-//     return;
-//     ` <section id="country-details">
-//         <button id="btn-leave-details" aria-label="back to main page">
-//           <i class="bi bi-arrow-left"></i> Back
-//         </button>
 
-//         <div id="country-details-container">
-//           <div>
-//             <img id="country-detail-flag" src="#" alt="flag not loaded" />
-//           </div>
-
-//           <div class="country-detail-text">
-//             <h2 id="country-detail-name">${item.name.common}</h2>
-
-//             <div class="country-detail-text-container">
-//               <div class="text-container1">
-//                 <p>
-//                   <span>Native Name: </span>
-//                   <span id="detail-native-name"></span>
-//                 </p>
-//                 <p>
-//                   <span>Population: </span>
-//                   <span id="detail-population"></span>
-//                 </p>
-//                 <p><span>Region: </span><span id="detail-region"></span></p>
-//                 <p>
-//                   <span>Sub Region: </span>
-//                   <span id="detail-sub-region"></span>
-//                 </p>
-//                 <p><span>Capital: </span><span id="detail-capital">Ul</span></p>
-//               </div>
-//               <div class="text-container2">
-//                 <p>
-//                   <span>Top Level Domain: </span>
-//                   <span id="detail-tld"></span>
-//                 </p>
-//                 <p>
-//                   <span>Currencies: </span>
-//                   <span id="detail-currency"></span>
-//                 </p>
-//                 <p>
-//                   <span>Languages: </span>
-//                   <span id="detail-languages"></span>
-//                 </p>
-//               </div>
-//             </div>
-
-//             <h3>Border Countries:</h3>
-//             <div id="border-countries-container"></div>
-//           </div>
-//         </div>
-//       </section>`;
-//   });
-//   countryDetailsContainer.insertAdjacentHTML(
-//     "beforeend",
-//     countriesHtml.join("")
-//   );
-
-//   // Finalmente, removemos la clase "display-none" para mostrar el elemento #country-details
-//   countryDetails.classList.remove("display-none");
-// }
-// getCountry();
-
+ let data =[];
 async function getCountry() {
   const respuesta = await fetch("https://restcountries.com/v3.1/all");
-  const data = await respuesta.json();
+  data = await respuesta.json();
 
   const countryDetails = document.querySelector("#country-details");
   const countryDetailsContainer = document.querySelector(
@@ -99,8 +24,7 @@ async function getCountry() {
 
   // Aquí utilizamos el método map en lugar de forEach para crear un array con el contenido HTML para cada país
   const countriesHtml = data.map((item) => {
-    console.log(item);
-    console.log(item.flags[0])
+    //console.log(item);
     return `
     <div class="text-container1">
     <div>
@@ -139,3 +63,56 @@ async function getCountry() {
   countryDetails.classList.remove("display-none");
 }
 getCountry();
+
+
+// Obtén una referencia al campo de búsqueda
+const searchInput = document.getElementById("input-field-country");
+
+
+function mostrarResultados(resultados) {
+  const countryDetailsContainer = document.querySelector("#countries-selection-box");
+  countryDetailsContainer.innerHTML = ""; // Limpiar el contenido actual antes de mostrar los nuevos resultados
+
+  resultados.forEach(item => {
+    const countryHtml = `
+      <div class="text-container1">
+        <div>
+          <img id="country-detail-flag" src="${item.flags.png}" alt="flag not loaded" />
+        </div>
+        <p>
+          <span>Native Name: ${item.name.common}</span>
+          <span id="detail-native-name"></span>
+        </p>
+        <p>
+          <span>Population: ${item.population}</span>
+          <span id="detail-population"></span>
+        </p>
+        <p><span>Region: </span><span id="detail-region">${item.region}</span></p>
+        <p>
+          <span>Sub Region:  </span>
+          <span id="detail-sub-region">${item.subregion}</span>
+        </p>
+        <p><span>Capital:${item.capital} </span><span id="detail-capital">Ul</span></p>
+      </div>
+    `;
+
+    countryDetailsContainer.insertAdjacentHTML("beforeend", countryHtml);
+  });
+}
+
+// Agrega un event listener para el evento "input"
+searchInput.addEventListener('input', function(event) {
+  // Acciones a realizar cuando se dispara el evento "input"
+  const textoIngresado = event.target.value;
+  console.log("Texto ingresado:", textoIngresado);
+  // Realizar la búsqueda o actualizar los resultados aquí
+  const resultados = data.filter(item => {
+  
+    const nombreComun = item.name.common.toLowerCase();
+    return nombreComun.includes(textoIngresado);
+   
+
+  });
+ 
+  mostrarResultados(resultados);
+});
